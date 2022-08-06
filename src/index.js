95711afd2b580c7c7769d0ea775636431d606d8b
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2022-08-06 07:46:57 -0400

    add copy

--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
-import { 
-    textoIngresado, 
-    textoResultado, 
-    contenedorResult,
-    draw, ningunMensaje, message 
+import {
+  textoIngresado,
+  textoResultado,
+  contenedorResult,
+  draw,
+  ningunMensaje,
+  message
 } from "./modules/elementos.js";
-import { botonDesencriptar, botonEncriptar} from "./modules/elementos-button.js";
+import {
+  botonDesencriptar,
+  botonEncriptar,
+  btnCopiar
+} from "./modules/elementos-button.js";
 import desencriptar from "./modules/desencriptador.js";
 import encriptarTexto from "./modules/encriptador.js";
 
-textoIngresado.addEventListener('click', () => {
-    textoIngresado.value = ''
-    draw.style.display = 'none'
-    ningunMensaje.style.display = 'none'
-    message.style.display = 'none'
-    contenedorResult.style.display= 'inline'
-})
+textoIngresado.addEventListener("click", () => {
+  textoIngresado.value = "";
+  draw.style.display = "none";
+  ningunMensaje.style.display = "none";
+  message.style.display = "none";
+  contenedorResult.style.display = "inline";
+});
 
-botonEncriptar.addEventListener('click', () => {
-    textoResultado.innerText = encriptarTexto(textoIngresado.value)
-})
+botonEncriptar.addEventListener("click", () => {
+  textoResultado.innerText = encriptarTexto(textoIngresado.value);
+});
 
-botonDesencriptar.addEventListener('click', () => {
-    textoResultado.innerText = desencriptar(textoIngresado.value)
-})
+botonDesencriptar.addEventListener("click", () => {
+  textoResultado.innerText = desencriptar(textoIngresado.value);
+});
+
+btnCopiar.addEventListener("click", () => {
+  if (!navigator.clipboard) {
+    // use old commandExec() way
+  } else {
+    navigator.clipboard.writeText(textoResultado.innerText)
+    .then(function () {
+        alert("el texto a sido copiado!"); // success
+    })
+    .catch(function () {
+        alert("error al copiar texto!"); // error
+    });
+  }
+});
--- src/modules/elementos-button.js
+++ src/modules/elementos-button.js
@@ -1,7 +1,9 @@
 const botonEncriptar = document.getElementById("encriptar")
 const botonDesencriptar = document.getElementById("desencriptar")
+const btnCopiar = document.getElementById("copiar")
 
 export {    
     botonEncriptar,
-    botonDesencriptar
+    botonDesencriptar,
+    btnCopiar
 }
