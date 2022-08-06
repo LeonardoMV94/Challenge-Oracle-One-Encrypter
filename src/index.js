c26ee9f4e289207abfa1dc510432fe9f299cc40b
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2022-08-06 09:55:31 -0400

    add event keyup texarea

--- src/index.js
+++ src/index.js
@@ -15,12 +15,20 @@ import desencriptar from "./modules/desencriptador.js";
 import encriptarTexto from "./modules/encriptador.js";
 
 textoIngresado.addEventListener("click", () => {
-  textoIngresado.value = "";
-  draw.style.display = "none";
-  ningunMensaje.style.display = "none";
-  message.style.display = "none";
-  contenedorResult.style.display = "inline";
+  textoIngresado.value = "";  
 });
+textoIngresado.addEventListener('keyup', () => {
+    if (textoIngresado.value.length > 0) {
+        draw.style.display = "none";
+        ningunMensaje.style.display = "none";
+        contenedorResult.style.display = "inline";
+    } else {
+        draw.style.display = "inline";
+        ningunMensaje.style.display = "inline";
+        // message.style.display = "none";
+        contenedorResult.style.display = "none";
+    }
+})
 
 botonEncriptar.addEventListener("click", () => {
   textoResultado.innerText = encriptarTexto(textoIngresado.value);
@@ -33,13 +41,17 @@ botonDesencriptar.addEventListener("click", () => {
 btnCopiar.addEventListener("click", () => {
   if (!navigator.clipboard) {
     // use old commandExec() way
+    textoResultado.select();
+    document.execCommand("copy");
+    alert("El texto a sido copiado!");
   } else {
-    navigator.clipboard.writeText(textoResultado.innerText)
-    .then(function () {
-        alert("el texto a sido copiado!"); // success
-    })
-    .catch(function () {
-        alert("error al copiar texto!"); // error
-    });
+    navigator.clipboard
+      .writeText(textoResultado.innerText)
+      .then(function () {
+        alert("El texto a sido copiado!"); // success
+      })
+      .catch(function () {
+        alert("Error al copiar texto!"); // error
+      });
   }
 });
